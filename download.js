6bc61fcaef49f69ae51b9d24a390e917f6a745aa
Gary Irick <garyirick@gmail.com>
2023-12-30 20:04:12 -0600

    Added code to detect players with camera hijinks.

--- download.js
+++ download.js
@@ -1,7 +1,11 @@
 const process = require('process')
 const fs = require('fs')
 const agent = require('superagent')
+const heroprotocol = require('heroprotocol')
 const { ngsApiUrl, replayBucket } = require('./config')
+
+const REPLAYS_DIR = 'replays'
+const CAMERA_FILE = 'suspicious-cameras.csv'
 
 const getMatches = async (season) => {
   const result = await (
@@ -12,12 +16,44 @@ const getMatches = async (season) => {
   return result.body.returnObject
 }
 
-const copyReplayIfNeeded = async (season, division, filename) => {
+const checkForBadCamera = (match, replayUrl, replayFilename) => {
+  const initData = heroprotocol.get('replay.initdata', replayFilename)
+  const details = heroprotocol.get('replay.details', replayFilename)
+  const gameEvents = heroprotocol.get('replay.game.events', replayFilename)
+
+  const players = details.m_playerList.map(p => p.m_name)
+
+  let found = false
+
+  for (const gameEvent of gameEvents) {
+    if (gameEvent._event === 'NNet.Game.SCameraUpdateEvent') {
+      const distance = gameEvent.m_distance
+
+      if (distance > 8704) {
+        const userId = gameEvent._userid.m_userId
+        const name = initData.m_syncLobbyState.m_userInitialData[userId].m_name
+
+        if (players.findIndex(p => p === name) > -1) {
+          const time = (gameEvent._gameloop - 610) / 16
+          fs.appendFileSync(CAMERA_FILE, `${replayUrl},${match.season},${match.divisionConcat},${match.home.teamName},${match.away.teamName},${name},${time},${distance}\n`)
+
+          if (!found) {
+            console.log('Found suspicious camera distance.')
+            found = true
+          }
+        }
+      }
+    }
+  }
+}
+
+const copyReplayIfNeeded = async (season, match, filename) => {
+  const division = match.divisionConcat
   const parts = division.split('-')
-  const divisionSubdir = parts.length == 2 ? `${parts[0]}/${parts[0]}-${parts[1]}` : parts[0]
-  const directory = `replays/${season}/${divisionSubdir}`
+  const divisionSubdir = parts.length === 2 ? `${parts[0]}/${parts[0]}-${parts[1]}` : parts[0]
+  const directory = `${REPLAYS_DIR}/${season}/${divisionSubdir}`
   const fullPath = `${directory}/${filename}`
-  let fullUrl = `https://s3.amazonaws.com/${replayBucket}/${filename}`
+  const fullUrl = `https://s3.amazonaws.com/${replayBucket}/${filename}`
 
   if (fs.existsSync(fullPath)) {
     return 0
@@ -26,9 +62,11 @@ const copyReplayIfNeeded = async (season, division, filename) => {
   fs.mkdirSync(directory, { recursive: true })
 
   try {
-    download = await agent.get(fullUrl)
+    const download = await agent.get(fullUrl).responseType('blob')
     fs.writeFileSync(fullPath, download.body)
     console.log(`Downloaded ${filename} for ${division}.`)
+
+    checkForBadCamera(match, fullUrl, fullPath)
   } catch (e) {
     console.log(`Unable to download ${filename} for ${division}.`)
     return 0
@@ -38,11 +76,19 @@ const copyReplayIfNeeded = async (season, division, filename) => {
 }
 
 const run = async (season, divisionPrefix) => {
+  if (!fs.existsSync(CAMERA_FILE)) {
+    fs.writeFileSync(CAMERA_FILE, 'URL,Season,Division,Home,Away,Player,Time,Distance\n')
+  }
+
   const matches = await getMatches(season)
   let count = 0
 
   for (const match of matches) {
-    if (!match.divisionConcat || divisionPrefix !== '' && !match.divisionConcat.toLowerCase().startsWith(divisionPrefix.toLowerCase())) {
+    if (!match.divisionConcat) {
+      continue
+    }
+
+    if (divisionPrefix !== '' && !match.divisionConcat.toLowerCase().startsWith(divisionPrefix.toLowerCase())) {
       continue
     }
 
@@ -55,7 +101,7 @@ const run = async (season, divisionPrefix) => {
         const filename = match.replays[i].url
 
         if (filename) {
-          count += await copyReplayIfNeeded(season, match.divisionConcat, filename)
+          count += await copyReplayIfNeeded(season, match, filename)
         }
       }
     }
@@ -70,7 +116,7 @@ let divisionPrefix = ''
 if (process.argv.length >= 3 && process.argv.length <= 4) {
   try {
     season = Number(process.argv[2])
-  } catch {
+  } catch (e) {
   }
 
   divisionPrefix = process.argv.length === 4 ? process.argv[3] : ''
